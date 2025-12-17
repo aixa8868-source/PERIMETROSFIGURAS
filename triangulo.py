31716805f4a480f3d4c5a433486483478895bd5b
aixa-source <aixa8868@gmail.com>
2025-12-17 17:45:42 -0600

    PERIMETRO DEL TRIANGULO MILDRED AZUL CARDENAS LOPEZ

---
+++ triangulo.py
@@ -0,0 +1,6 @@
+a = float(input("Ingresa el lado a: "))
+b = float(input("Ingresa el lado b: "))
+c = float(input("Ingresa el lado c: "))
+
+perimetro = a + b + c
+print("El perímetro del triángulo es:", perimetro)
